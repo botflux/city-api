79c0c337d092741a850e96b7d8e777dd1e971fb9
Victor Mendele <victor.mendele68@gmail.com>
2019-01-30 15:50:07 +0100

    fix: typo

--- src/convert-counties.js
+++ src/convert-counties.js
@@ -1,3 +1,8 @@
+/**
+ * Converts all counties in a webchasse compatible format.
+ * 
+ * @param {[]} counties list of counties
+ */
 const convertCounties = (counties) => {
     return counties.map(c => {
         const { codeRegion, nom } = c
--- src/search-counties.js
+++ src/search-counties.js
@@ -1,4 +1,9 @@
 
+/**
+ * Fetch all the counties
+ * 
+ * @param {Function} fetch function used for fetching data
+ */
 const searchCounties = (fetch) => {
     return fetch(`https://geo.api.gouv.fr/departements`)
         .then(response => response.json())
