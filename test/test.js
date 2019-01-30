3856dff29dca03c79e87df0092460742bc3c5916
Victor Mendele <victor.mendele68@gmail.com>
2019-01-30 15:29:31 +0100

    feat: test for searchCounties

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 const assert = require('assert')
 const searchCounty = require('../src/search-county')
 const convertCities = require('../src/convert-cities')
+const searchCounties = require('../src/search-counties')
 
 // this flag is here to know if the fake fetch has been called
 let isFakeFetchCalled = false
 
 // mocking a fake request
-const fakeFetch = (url) => {
+const fakeFetchCounty = (url) => {
     assert.strictEqual(url, 'https://geo.api.gouv.fr/departements/68/communes')
 
     isFakeFetchCalled = true
@@ -20,16 +21,57 @@ const fakeFetch = (url) => {
     })
 }
 
+// mocking a fake request
+const fakeFetchCounties = (url) => {
+    assert.strictEqual(url, 'https://geo.api.gouv.fr/departements')
+
+    isFakeFetchCalled = true
+
+    return Promise.resolve({
+        json: () => {
+            return Promise.resolve([
+                { nom: 'Bergheim', code: '68028', codeDepartement: '68', codeRegion: '44', codesPostaux: [ '68750' ], population: 1986 }
+            ])
+        }
+    })
+}
+
 describe('#searchCounty', () => {
+    // reset fake fetch state before testing
+    isFakeFetchCalled = false
+
     it ('calls geo.api.gouv.fr', () => {
-        searchCounty(fakeFetch, { county: '68' })
+        searchCounty(fakeFetchCounty, { county: '68' })
             .then(result => {
                 assert.strictEqual(isFakeFetchCalled, true)
             })
     })
 
     it ('returns the list of cities', (done) => {
-        searchCounty(fakeFetch, { county: '68' })
+        searchCounty(fakeFetchCounty, { county: '68' })
+            .then(result => {
+                assert.strictEqual(
+                    JSON.stringify(result[0]), 
+                    JSON.stringify({ nom: 'Bergheim', code: '68028', codeDepartement: '68', codeRegion: '44', codesPostaux: [ '68750' ], population: 1986 }))
+                done()
+            })
+    })
+})
+
+describe('#searchCounties', () => {
+    // reset fake fetch called before testing it
+    isFakeFetchCalled = false
+
+    it ('calls geo.api.gouv.fr', (done) => {
+        searchCounties(fakeFetchCounties)
+            .then(res => {
+                assert.strictEqual(isFakeFetchCalled, true)
+                done()
+            })
+    })
+    
+    it ('returns the list of cities', (done) => {
+        searchCounties(fakeFetchCounties)
             .then(result => {
                 assert.strictEqual(
                     JSON.stringify(result[0]), 
