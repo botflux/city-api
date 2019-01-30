539949bcf21e3bb2728d1e39705e89230207aa41
Victor Mendele <victor.mendele68@gmail.com>
2019-01-30 15:57:03 +0100

    feat: county route

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const searchCounty = require('./src/search-county')
 const convertCities = require('./src/convert-cities')
+const searchCounties = require('./src/search-counties')
+const convertCounties = require('./src/convert-counties')
 const fetch = require('node-fetch')
 
 const APPLICATION_PORT = process.env.PORT || 3000
 const app = express()
+
+app.get('/county', (req, res) => {
+    searchCounties(fetch)
+        .then(geoCounties => convertCounties(geoCounties))
+        .then(convertedCounties => ({
+            errors: false,
+            results: convertedCounties
+        }))
+        .then(response => res.send(JSON.stringify(response)))
+        .catch(e => res.send(JSON.stringify({
+            errors: true,
+            result: JSON.stringify(e)
+        })))
+})
 
 app.get('/county/:code', (req, res) => {
     const { code } = req.params
